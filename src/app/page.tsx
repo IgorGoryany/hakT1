984ea1d7c6437dede7c99eb8199233f8a03c1d6f
Igor Goryany <128911252+IgorGoryany@users.noreply.github.com>
2024-11-25 14:19:22 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { TextEditorPage } from '@/components/TextEditorPage';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background pt-20 text-4">
+      <TextEditorPage />
+    </div>
+  );
+};
+
+export default Home;
